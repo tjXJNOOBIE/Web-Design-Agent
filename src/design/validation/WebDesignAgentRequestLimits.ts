5483785cd6d21322dd1e451008bcb507a5698d69
TJ <tjxjnoobie@gmail.com>
2026-09-08 14:18:12 -0700

    Added: define public design request limits

---
+++ src/design/validation/WebDesignAgentRequestLimits.ts
@@ -0,0 +1,10 @@
+export const WEB_DESIGN_AGENT_REQUEST_LIMITS = Object.freeze({
+  promptCharacters: 12_000,
+  feedbackCharacters: 6_000,
+  urlCharacters: 2_048,
+  pageCount: 12,
+  pagePathCharacters: 256,
+  candidateSerializedCharacters: 500_000,
+  preferenceProfileSerializedCharacters: 100_000,
+  selectedConceptSerializedCharacters: 16_000,
+})
