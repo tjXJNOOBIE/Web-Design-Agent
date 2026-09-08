1df2e266afa3c0eb0db86ff7fa6dd64840b4d808
TJ <tjxjnoobie@gmail.com>
2026-09-08 14:26:29 -0700

    Removed: drop unenforced request limit constants

--- src/design/validation/WebDesignAgentRequestLimits.ts
+++ src/design/validation/WebDesignAgentRequestLimits.ts
@@ -4,7 +4,4 @@ export const WEB_DESIGN_AGENT_REQUEST_LIMITS = Object.freeze({
   urlCharacters: 2_048,
   pageCount: 12,
   pagePathCharacters: 256,
-  candidateSerializedCharacters: 500_000,
-  preferenceProfileSerializedCharacters: 100_000,
-  selectedConceptSerializedCharacters: 16_000,
 })
