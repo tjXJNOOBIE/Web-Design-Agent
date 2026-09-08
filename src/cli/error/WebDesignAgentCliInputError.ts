41900dc2fac52748465d799ed66f383c80031558
TJ <tjxjnoobie@gmail.com>
2026-09-08 13:08:22 -0700

    Changed: support precise CLI input errors

--- src/cli/error/WebDesignAgentCliInputError.ts
+++ src/cli/error/WebDesignAgentCliInputError.ts
@@ -1,1 +1,8 @@
-export class WebDesignAgentCliInputError extends Error{public constructor(){super('Web Design Agent requires a non-blank request.');this.name='WebDesignAgentCliInputError'}}
+export class WebDesignAgentCliInputError extends Error {
+  public constructor(
+    message = 'Web Design Agent requires a non-blank request.',
+  ) {
+    super(message)
+    this.name = 'WebDesignAgentCliInputError'
+  }
+}
